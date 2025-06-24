aa89cb809f936147788eee3d26827947bb3599a0
benz <benzaria26@gmail.com>
2025-06-24 15:53:31 +0100

    change hardcoded userID 🤦‍♂️

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 echo > "%temp%\msgbox"
 for /f %%i in (' dir /b RunBash*.exe ') do set "RunBash=%%~fi"
-"%RunBash%" -Install " " "C:\Users\benz\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+"%RunBash%" -Install " " "%home%\AppData\Local\Programs\Microsoft VS Code\Code.exe"
 "%RunBash%" -GetBin 
 "%RunBash%" -AddBin ls ls --color=auto ; ll ls --color=auto -l ; la ls --color=auto -a ; lla ls --color=auto -la ; l ls --color=auto -CF
 "%RunBash%" -AddBin grep grep --color=auto ; fgrep fgrep --color=auto ; egrep egrep --color=auto ; ip ip --color=auto ; diff diff --color=auto
